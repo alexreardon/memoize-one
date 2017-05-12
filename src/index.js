d2194332cc330cc66be5c568b43d197bf10fa7d3
Alex Reardon <areardon@atlassian.com>
2017-05-12 17:03:15 +1000

    hardening up the flow types so that the memoized function is the same type as the function passed in

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 // @flow
-type EqualityFn = (a: any, b: any) => boolean;
+type EqualityFn = (a: mixed, b: mixed) => boolean;
 
-const simpleIsEqual = (a: any, b: any): boolean => a === b;
+const simpleIsEqual: EqualityFn = (a: mixed, b: mixed): boolean => a === b;
 
-export default function (resultFn: Function, isEqual?: EqualityFn = simpleIsEqual) {
-  let lastThis: any;
-  let lastArgs: Array<any> = [];
-  let lastResult: any;
+export default function <ResultFn: (...Array<any>) => mixed>(resultFn: ResultFn, isEqual?: EqualityFn = simpleIsEqual): ResultFn {
+  let lastThis: mixed;
+  let lastArgs: Array<mixed> = [];
+  let lastResult: mixed;
   let calledOnce: boolean = false;
 
-  const isNewArgEqualToLast = (newArg, index) => isEqual(newArg, lastArgs[index]);
+  const isNewArgEqualToLast = (newArg: mixed, index: number): boolean => isEqual(newArg, lastArgs[index]);
 
-    // breaking cache when context (this) or arguments change
-  return function (...newArgs: Array<any>) {
+  // breaking cache when context (this) or arguments change
+  const result = function (...newArgs: Array<mixed>) {
     if (calledOnce &&
-            lastThis === this &&
-            newArgs.length === lastArgs.length &&
-            newArgs.every(isNewArgEqualToLast)) {
+      lastThis === this &&
+      newArgs.length === lastArgs.length &&
+      newArgs.every(isNewArgEqualToLast)) {
       return lastResult;
     }
 
@@ -26,4 +26,7 @@ export default function (resultFn: Function, isEqual?: EqualityFn = simpleIsEqua
     lastResult = resultFn.apply(this, newArgs);
     return lastResult;
   };
+
+  // telling flow to ignore the type of `result` as we know it is `ResultFn`
+  return (result: any);
 }
