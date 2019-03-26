edc40d295d0e5d5299465799dd7932ce609ecd4f
Alex Reardon <areardon@atlassian.com>
2019-03-27 10:17:05 +1100

    bumping deps. updating style and docs

--- src/index.js
+++ src/index.js
@@ -14,23 +14,24 @@ const simpleIsEqual: EqualityFn = (
       shallowEqual(newArg, lastArgs[index]),
   );
 
-// <ResultFn: (...Array<any>) => mixed>
+// <ResultFn: (...any[]) => mixed>
 // The purpose of this typing is to ensure that the returned memoized
 // function has the same type as the provided function (`resultFn`).
 // ResultFn:        Generic type (which is the same as the resultFn).
-// (...Array<any>): Accepts any length of arguments - and they are not checked
+// (...any[]): Accepts any length of arguments - and they are not checked
 // mixed:           The result can be anything but needs to be checked before usage
-export default function<ResultFn: (...Array<any>) => mixed>(
+export default function<ResultFn: (...any[]) => mixed>(
   resultFn: ResultFn,
   isEqual?: EqualityFn = simpleIsEqual,
 ): ResultFn {
   let lastThis: mixed;
-  let lastArgs: Array<mixed> = [];
+  let lastArgs: mixed[] = [];
   let lastResult: mixed;
   let calledOnce: boolean = false;
 
   // breaking cache when context (this) or arguments change
-  const result = function(...newArgs: Array<mixed>) {
+  // $ExpectError - faking the type of ResultFn
+  const result: ResultFn = function(...newArgs: mixed[]) {
     if (calledOnce && lastThis === this && isEqual(newArgs, lastArgs)) {
       return lastResult;
     }
@@ -45,6 +46,5 @@ export default function<ResultFn: (...Array<any>) => mixed>(
     return lastResult;
   };
 
-  // telling flow to ignore the type of `result` as we know it is `ResultFn`
-  return (result: any);
+  return result;
 }
