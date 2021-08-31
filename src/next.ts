be62c9127fe08888e9a74ccbf531ee7a06c16843
Alex Reardon <alexreardon@gmail.com>
2021-08-31 12:29:52 +1000

    typing goodness

--- src/next.ts
+++ src/next.ts
@@ -3,14 +3,14 @@ import areInputsEqual from './are-inputs-equal';
 // Using ReadonlyArray<T> rather than readonly T as it works with TS v3
 export type EqualityFn = (newArgs: any[], lastArgs: any[]) => boolean;
 
-type State = {
+type State<TResult> = {
   lastThis: unknown;
   lastArgs: unknown[];
-  lastResult: unknown;
+  lastResult: TResult | undefined;
   calledOnce: boolean;
 };
 
-function getInitialState(): State {
+function getInitialState<TResult>(): State<TResult> {
   return {
     lastThis: undefined,
     lastArgs: [],
@@ -19,18 +19,24 @@ function getInitialState(): State {
   };
 }
 
+type MemoizedFn<T extends (this: any, ...args: any[]) => any> = {
+  name: string;
+  clear: () => void;
+  (...args: Parameters<T>): ReturnType<T>;
+};
+
 function memoizeOne<
   // Need to use 'any' rather than 'unknown' here as it has
   // The correct Generic narrowing behaviour.
-  ResultFn extends (this: any, ...newArgs: any[]) => ReturnType<ResultFn>
->(resultFn: ResultFn, isEqual: EqualityFn = areInputsEqual): ResultFn {
-  let state: State = getInitialState();
+  TFunc extends (this: any, ...newArgs: any[]) => any
+>(resultFn: TFunc, isEqual: EqualityFn = areInputsEqual): MemoizedFn<TFunc> {
+  let state: State<ReturnType<TFunc>> = getInitialState();
 
   // breaking cache when context (this) or arguments change
-  function memoized(this: unknown, ...newArgs: unknown[]): ReturnType<ResultFn> {
+  function memoized(this: unknown, ...newArgs: unknown[]): ReturnType<TFunc> {
     const { lastThis, lastArgs, lastResult, calledOnce } = state;
     if (calledOnce && lastThis === this && isEqual(newArgs, lastArgs)) {
-      return lastResult;
+      return lastResult as ReturnType<TFunc>;
     }
 
     // Throwing during an assignment aborts the assignment: https://codepen.io/alexreardon/pen/RYKoaz
@@ -43,9 +49,13 @@ function memoizeOne<
       lastArgs: newArgs,
     };
 
-    return state.lastResult;
+    return state.lastResult as ReturnType<TFunc>;
   }
 
+  // Giving the function a better name for devtools
+  memoized.name = `memoized(${resultFn.name})`;
+
+  // Adding the ability to clear the cache of a memoized function
   memoized.clear = function clear() {
     state = getInitialState();
   };
