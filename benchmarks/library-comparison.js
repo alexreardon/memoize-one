b615c7757228a58a855771b1c8a48b95da0557a1
Alex Reardon <areardon@atlassian.com>
2021-09-27 11:34:22 +1000

    leaning into ts

--- benchmarks/library-comparison.js
+++ benchmarks/library-comparison.js
@@ -1,113 +1,106 @@
 /* eslint-disable no-console */
 import benchmark from 'benchmark';
-import memoizeOne from '../dist/memoize-one.esm.js';
+import memoizeOne from '../dist/memoize-one.esm';
 import lodash from 'lodash.memoize';
 import fastMemoize from 'fast-memoize';
 import mem from 'mem';
 import ora from 'ora';
 import { green, bold } from 'nanocolors';
-
-const libraries = [
-  {
-    name: 'no memoization',
-    memoize: (fn) => fn,
-  },
-  {
-    name: 'memoize-one',
-    memoize: memoizeOne,
-  },
-  {
-    name: 'lodash.memoize',
-    memoize: lodash,
-  },
-  {
-    name: 'fast-memoize',
-    memoize: fastMemoize,
-  },
-  {
-    name: 'mem',
-    memoize: mem,
-  },
+var libraries = [
+    {
+        name: 'no memoization',
+        memoize: function (fn) { return fn; }
+    },
+    {
+        name: 'memoize-one',
+        memoize: memoizeOne
+    },
+    {
+        name: 'lodash.memoize',
+        memoize: lodash
+    },
+    {
+        name: 'fast-memoize',
+        memoize: fastMemoize
+    },
+    {
+        name: 'mem',
+        memoize: mem
+    },
 ];
-
 function slowFn() {
-  for (let i = 0; i < 2000; i++) {
-    void undefined;
-  }
+    for (var i = 0; i < 2000; i++) {
+        void undefined;
+    }
 }
-
-const cases = [
-  {
-    name: 'no arguments',
-    baseFn: slowFn,
-    args: [],
-  },
-  // {
-  //   name: 'single primitive argument',
-  //   baseFn: function add1(value) {
-  //     slowFn();
-  //     return value + 1;
-  //   },
-  //   args: [2],
-  // },
-  // {
-  //   name: 'single complex argument',
-  //   baseFn: function identity(value) {
-  //     slowFn();
-  //     return value;
-  //   },
-  //   args: [{ hello: 'world' }],
-  // },
-  // {
-  //   name: 'multiple primitive arguments',
-  //   baseFn: function asArray(...values) {
-  //     slowFn();
-  //     return values;
-  //   },
-  //   args: [1, 'hello', true],
-  // },
-  // {
-  //   name: 'multiple complex arguments',
-  //   baseFn: function asArray(...values) {
-  //     slowFn();
-  //     return values;
-  //   },
-  //   args: [() => {}, { hello: { there: 'world' } }, [1, 2, 3]],
-  // },
+var scenarios = [
+    {
+        name: 'no arguments',
+        baseFn: slowFn,
+        args: []
+    },
+    // {
+    //   name: 'single primitive argument',
+    //   baseFn: function add1(value) {
+    //     slowFn();
+    //     return value + 1;
+    //   },
+    //   args: [2],
+    // },
+    // {
+    //   name: 'single complex argument',
+    //   baseFn: function identity(value) {
+    //     slowFn();
+    //     return value;
+    //   },
+    //   args: [{ hello: 'world' }],
+    // },
+    // {
+    //   name: 'multiple primitive arguments',
+    //   baseFn: function asArray(...values) {
+    //     slowFn();
+    //     return values;
+    //   },
+    //   args: [1, 'hello', true],
+    // },
+    // {
+    //   name: 'multiple complex arguments',
+    //   baseFn: function asArray(...values) {
+    //     slowFn();
+    //     return values;
+    //   },
+    //   args: [() => {}, { hello: { there: 'world' } }, [1, 2, 3]],
+    // },
 ];
-
-cases.forEach((useCase) => {
-  const suite = new benchmark.Suite(useCase.name);
-
-  libraries.forEach(function callback(library) {
-    const memoized = library.memoize(useCase.baseFn);
-    const spinner = ora({
-      text: library.name,
-      spinner: {
-        frames: ['⏳'],
-      },
+scenarios.forEach(function (useCase) {
+    var suite = new benchmark.Suite(useCase.name);
+    libraries.forEach(function callback(library) {
+        var memoized = library.memoize(useCase.baseFn);
+        var spinner = ora({
+            text: library.name,
+            spinner: {
+                frames: ['⏳']
+            }
+        });
+        // Add a benchmark
+        suite.add({
+            name: library.name,
+            fn: function () { return memoized.apply(void 0, useCase.args); },
+            onStart: function () { return spinner.start(); },
+            onComplete: function () { return spinner.succeed(); }
+        });
     });
-
-    // Add a benchmark
-    suite.add({
-      name: library.name,
-      fn: () => memoized(...useCase.args),
-      onStart: () => spinner.start(),
-      onComplete: () => spinner.succeed(),
+    suite.on('start', function () {
+        console.log(bold('Scenario') + ": " + green(useCase.name));
     });
-  });
-
-  suite.on('start', () => {
-    console.log(`${bold('Scenario')}: ${green(useCase.name)}`);
-  });
-  // suite.on('cycle', (e) => console.log(String(e.target)));
-  suite.on('complete', (event) => {
-    const map = event.target;
-    const benchmarks = Object.values(map);
-    // benchmarks.sort((a, b) => {
-    //   return a.hz > b.hz;
-    // });
-    // console.log(benchmarks);
-  });
-  suite.run();
+    // suite.on('cycle', (e) => console.log(String(e.target)));
+    suite.on('complete', function (event) {
+        var map = event.target;
+        var benchmarks = Object.values(map);
+        // benchmarks.sort((a, b) => {
+        //   return a.hz > b.hz;
+        // });
+        // console.log(benchmarks);
+    });
+    suite.run();
 });
---
+++ benchmarks/library-comparison.ts
@@ -0,0 +1,135 @@
+/* eslint-disable no-console */
+import benchmark from 'benchmark';
+import memoizeOne from '../dist/memoize-one.esm';
+import lodash from 'lodash.memoize';
+import fastMemoize from 'fast-memoize';
+import mem from 'mem';
+import ora from 'ora';
+import { green, bold } from 'nanocolors';
+
+type Library = {
+  name: string;
+  memoize: (fn: (...args: any[]) => unknown) => (...args: any[]) => unknown;
+};
+const libraries: Library[] = [
+  {
+    name: 'no memoization',
+    memoize: (fn) => fn,
+  },
+  {
+    name: 'memoize-one',
+    memoize: memoizeOne,
+  },
+  {
+    name: 'lodash.memoize',
+    memoize: lodash,
+  },
+  {
+    name: 'fast-memoize',
+    memoize: fastMemoize,
+  },
+  {
+    name: 'mem',
+    memoize: mem,
+  },
+];
+
+function slowFn(): void {
+  for (let i = 0; i < 2000; i++) {}
+}
+
+type Scenario<TArgs> = {
+  name: string;
+  baseFn: (...args: TArgs[]) => unknown;
+  args: TArgs[];
+};
+
+// const first: Scenario<void> = {
+//   name: 'no arguments',
+//   baseFn: slowFn,
+//   args: [],
+// };
+// const second: Scenario<number> = {
+//   name: 'single primitive argument',
+//   baseFn: function add1(value) {
+//     slowFn();
+//     return value + 1;
+//   },
+//   args: [2],
+// };
+
+const scenarios: Scenario[] = [
+  {
+    name: 'no arguments',
+    baseFn: slowFn,
+    args: [],
+  },
+  // {
+  //   name: 'single primitive argument',
+  //   baseFn: function add1(value) {
+  //     slowFn();
+  //     return value + 1;
+  //   },
+  //   args: [2],
+  // },
+  // {
+  //   name: 'single complex argument',
+  //   baseFn: function identity(value) {
+  //     slowFn();
+  //     return value;
+  //   },
+  //   args: [{ hello: 'world' }],
+  // },
+  // {
+  //   name: 'multiple primitive arguments',
+  //   baseFn: function asArray(...values) {
+  //     slowFn();
+  //     return values;
+  //   },
+  //   args: [1, 'hello', true],
+  // },
+  // {
+  //   name: 'multiple complex arguments',
+  //   baseFn: function asArray(...values) {
+  //     slowFn();
+  //     return values;
+  //   },
+  //   args: [() => {}, { hello: { there: 'world' } }, [1, 2, 3]],
+  // },
+];
+
+scenarios.forEach((useCase) => {
+  const suite = new benchmark.Suite(useCase.name);
+
+  libraries.forEach(function callback(library) {
+    const memoized = library.memoize(useCase.baseFn);
+    const spinner = ora({
+      text: library.name,
+      spinner: {
+        frames: ['⏳'],
+      },
+    });
+
+    // Add a benchmark
+    suite.add({
+      name: library.name,
+      fn: () => memoized(...useCase.args),
+      onStart: () => spinner.start(),
+      onComplete: () => spinner.succeed(),
+    });
+  });
+
+  suite.on('start', () => {
+    console.log(`${bold('Scenario')}: ${green(useCase.name)}`);
+  });
+  // suite.on('cycle', (e) => console.log(String(e.target)));
+  suite.on('complete', (event: any) => {
+    const map = event.target;
+    const benchmarks = Object.values(map);
+    // benchmarks.sort((a, b) => {
+    //   return a.hz > b.hz;
+    // });
+    // console.log(benchmarks);
+  });
+  suite.run();
+});
