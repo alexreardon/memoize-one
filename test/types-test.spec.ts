82cf3af5f31aa8b3cd5bba14e99ae38528293dc5
Alex Reardon <alexreardon@gmail.com>
2021-10-01 20:35:08 +1000

    type test for equalityfn

--- test/types-test.spec.ts
+++ test/types-test.spec.ts
@@ -1,3 +1,4 @@
+import { EqualityFn } from './../src/memoize-one';
 import { expectTypeOf } from 'expect-type';
 import memoize from '../src/memoize-one';
 
@@ -26,4 +27,18 @@ it('should add a .clear function property', () => {
   expect(() => memoized.foo()).toThrow();
 
   expectTypeOf<typeof memoized.clear>().toEqualTypeOf<() => void>();
+});
+
+it('should type the equality function to based on the provided function', () => {
+  function add(first: number, second: number) {
+    return first + second;
+  }
+
+  expectTypeOf<EqualityFn<typeof add>>().toEqualTypeOf<
+    (newArgs: Parameters<typeof add>, lastArgs: Parameters<typeof add>) => boolean
+  >();
+
+  expectTypeOf<EqualityFn<typeof add>>().toEqualTypeOf<
+    (newArgs: [number, number], lastArgs: [number, number]) => boolean
+  >();
 });
