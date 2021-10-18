8b12bfbc271b95935aee4d1271dc0f9b8e14a9d9
Alex Reardon <alexreardon@gmail.com>
2021-10-18 16:06:09 +1100

    exporting MemoizedFn type

--- src/memoize-one.ts
+++ src/memoize-one.ts
@@ -5,15 +5,15 @@ export type EqualityFn<TFunc extends (...args: any[]) => any> = (
   lastArgs: Parameters<TFunc>,
 ) => boolean;
 
+export type MemoizedFn<TFunc extends (this: any, ...args: any[]) => any> = {
+  clear: () => void;
+  (this: ThisParameterType<TFunc>, ...args: Parameters<TFunc>): ReturnType<TFunc>;
+};
+
 type Cache<TFunc extends (this: any, ...args: any[]) => any> = {
   lastThis: ThisParameterType<TFunc>;
   lastArgs: Parameters<TFunc>;
   lastResult: ReturnType<TFunc>;
-};
-
-type MemoizedFn<TFunc extends (this: any, ...args: any[]) => any> = {
-  clear: () => void;
-  (this: ThisParameterType<TFunc>, ...args: Parameters<TFunc>): ReturnType<TFunc>;
 };
 
 function memoizeOne<TFunc extends (this: any, ...newArgs: any[]) => any>(
