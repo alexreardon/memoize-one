0eac58ed108579146b700ba570a3597a57c1ba08
Alex Reardon <alexreardon@gmail.com>
2021-10-27 19:13:27 +1100

    upgrading yarn lock

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 // @ts-check
-import typescript from 'rollup-plugin-typescript';
-import replace from 'rollup-plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
 const input = 'src/memoize-one.ts';
@@ -16,8 +16,8 @@ export default [
     },
     plugins: [
       // Setting development env before running other steps
-      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
-      typescript(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development'), preventAssignment: true }),
+      typescript({ module: 'ESNext' }),
     ],
   },
   // Universal module definition (UMD) build (production)
@@ -30,8 +30,8 @@ export default [
     },
     plugins: [
       // Setting production env before running other steps
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      typescript(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production'), preventAssignment: true }),
+      typescript({ module: 'ESNext' }),
       terser(),
     ],
   },
@@ -42,7 +42,7 @@ export default [
       file: 'dist/memoize-one.esm.js',
       format: 'esm',
     },
-    plugins: [typescript()],
+    plugins: [typescript({ module: 'ESNext' })],
   },
   // CommonJS build
   {
@@ -52,6 +52,6 @@ export default [
       format: 'cjs',
       exports: 'default',
     },
-    plugins: [typescript()],
+    plugins: [typescript({ module: 'ESNext' })],
   },
 ];
